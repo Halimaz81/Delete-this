bfe4cba307ad1b028b01181d48d9fb6072806faa
Keng Siong Toh <s10267107@connect.np.edu.sg>
2025-01-23 12:35:00 +0800

    Completed Task2, loaded Flights.csv

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using PRG2_Assignment;
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Testing");
+﻿using PRG2_Assignment;
+using PRG2_FinalAssignment;
 
 Terminal terminal = new Terminal("Terminal 5");
 void Loadfiles()// task 1
@@ -31,3 +28,69 @@ void Loadfiles()// task 1
 }
 
 Loadfiles();
+
+void LoadFlights(string flightscsv) //Task 2
+{
+    if (!File.Exists(flightscsv))
+    {
+        Console.WriteLine("File does not exists!");
+    }
+
+    try
+    {
+        using (StreamReader sr = new StreamReader(flightscsv))
+        {
+            sr.ReadLine();
+            string? data;
+            while ((data = sr.ReadLine()) != null)
+            {
+                string[] linedata = data.Split(",");
+
+                if (linedata.Length < 4)
+                {
+                    Console.WriteLine("Invalid flight data found, skipping..");
+                    continue;
+                }
+
+                string flightNumber = linedata[0];
+                string origin = linedata[1];
+                string destination = linedata[2];
+                DateTime expectedDepartureArrival = DateTime.ParseExact(linedata[3], "h:mm tt", null);
+                string? specialRequestCode = null;
+
+                if (linedata.Length == 5)
+                {
+                    specialRequestCode = linedata[4];
+                }
+
+                Flight flight;
+
+                if (specialRequestCode == "DDJB")
+                {
+                    flight = new DDJBFlight(flightNumber, origin, destination, expectedDepartureArrival, 300);
+                }
+                else if (specialRequestCode == "CFFT")
+                {
+                    flight = new CFFTFlight(flightNumber, origin, destination, expectedDepartureArrival, 150);
+                }
+                else if (specialRequestCode == "LWTT")
+                {
+                    flight = new LWTTFlight(flightNumber, origin, destination, expectedDepartureArrival, 500);
+                }
+                else
+                {
+                    flight = new NORMFlight(flightNumber, origin, destination, expectedDepartureArrival);
+                }
+
+                terminal.Flights[flight.FlightNumber] = flight;
+            }
+        }
+        Console.WriteLine("Flights Loaded successfully!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error loading flights: {ex.Message}");
+    }
+}
+
+LoadFlights("flights.csv");
