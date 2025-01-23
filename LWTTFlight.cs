f27c6ce4987b2af5bf77c216de4833608ee67cb1
Ryan Tan <ryantanzhong@gmail.com>
2025-01-23 12:12:04 +0800

    Created LWTTFlight class

---
+++ LWTTFlight.cs
@@ -0,0 +1,44 @@
+﻿using PRG2_Assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_FinalAssignment
+{
+    class LWTTFlight : Flight
+    {
+        public double RequestFee { get; set; }
+        public override double CalculateFees()
+        {
+            double extrafees = 0;
+            if (Origin == "Singapore (SIN)")//departing flights
+            {
+                extrafees += 800;
+            }
+            else if (Destination == "Singapore (SIN)") //arriving flights
+            {
+                extrafees += 500;
+            }
+
+            double totalfees = 300 + RequestFee + extrafees; //300 is the Boarding gate base fee
+            return totalfees;
+        }
+
+
+        public string Status = "On time";
+        public LWTTFlight(string flightNumber, string origin, string destination, DateTime expectedTime, double requestfee) : base(flightNumber, origin, destination, expectedTime)
+        {
+            RequestFee = requestfee;
+        }
+
+        public LWTTFlight() : base() { }
+        public override string ToString()
+        {
+            return base.ToString() + $"Request fee: {RequestFee}";
+        }
+
+
+    }
+}
