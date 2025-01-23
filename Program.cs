0e886fc4cd036047dd89d84cebd95e024db42843
Ryan Tan <ryantanzhong@gmail.com>
2025-01-23 12:31:08 +0800

    Completed task 1, and added the relevant csv files

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PRG2_Assignment;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Testing");
+
+Terminal terminal = new Terminal("Terminal 5");
+void Loadfiles()// task 1
+{
+    string[] airlines = File.ReadAllLines("airlines.csv");
+    Console.WriteLine("Loading Airlines...");
+    for (int i = 1; i < airlines.Length; i++)
+    {
+        string[] airlinedeets = airlines[i].Split(',');
+        Airline airlinex = new Airline(airlinedeets[0], airlinedeets[1]);
+        terminal.AddAirline(airlinex);
+    }
+    Console.WriteLine($"{airlines.Length} Airlines Loaded! ");
+
+    Console.WriteLine("Loading Boarding Gates...");
+    string[] boardinggates = File.ReadAllLines("boardinggates.csv");
+    for (int i = 1; i < boardinggates.Length; i++)
+    {
+        string[] boardingdeets = boardinggates[i].Split(",");
+        BoardingGate boardinggatex = new BoardingGate(boardingdeets[0], Convert.ToBoolean(boardingdeets[2]), Convert.ToBoolean(boardingdeets[1]), Convert.ToBoolean(boardingdeets[3])); //Boarding Gate,DDJB,CFFT,LWTT ,bool _supportscfft, bool _supportsddjb, bool _supportslwtt, Flight _flight)
+        terminal.AddBoardingGate(boardinggatex);
+    }
+    Console.WriteLine($"{boardinggates.Length} Boarding gates Loaded! ");
+
+    Console.WriteLine("Done");
+}
+
+Loadfiles();
